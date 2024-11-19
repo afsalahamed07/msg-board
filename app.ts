3e0661cfff24055a2b8fd048bea316e4597668c4
agrippa <afsalahamed07@gmail.com>
2024-11-19 14:50:54 +0530

    working homepage commit

--- app.ts
+++ app.ts
@@ -1,4 +1,34 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
+import path, { dirname } from "path";
 
+import * as dotenv from 'dotenv';
+import { fileURLToPath } from "url";
+import messages from "./db";
+dotenv.config()
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const assetPath = path.join(__dirname, "public");
+app.use(express.static(assetPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.log(err);
+	res.status(500).send(err)
+});
+
+app.get("/", (req: Request, res: Response) => {
+	res.render("index", { messages: messages })
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`First Express app - listnening on port ${PORT}`)
+})
+
---
+++ db.ts
@@ -0,0 +1,17 @@
+import Message from "types/Message";
+
+const messages: Message[] = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date()
+	},
+	{
+		text: "Hello World!",
+		user: "Charles",
+		added: new Date()
+	}
+];
+
+export default messages;
+
